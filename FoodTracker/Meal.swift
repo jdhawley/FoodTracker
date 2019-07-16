4e330ef1efd083314bf65a133782b8ce3e5fbbf6
jonathanhawley <jdhawley@crimson.ua.edu>
2019-07-15 23:05:21 -0400

    Added Meal.swift and some unit tests

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,33 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Jonathan Hawley on 7/15/19.
+//  Copyright © 2019 Jonathan Hawley. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    //MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int){
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
